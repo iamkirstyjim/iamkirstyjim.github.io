ab45556807d17480b25f91664f367ca9bd8ab631
Kirsty Jim <35421642+iamkirstyjim@users.noreply.github.com>
2024-01-27 21:02:58 +0800

    Format script.js

--- js/script.js
+++ js/script.js
@@ -1,36 +1,39 @@
 /* smooth scroll */
-$(document).ready(function(){ 
+$(document).ready(function () {
   /* Hide 'hide details' buttons in the beginning */
   $("#showLess1").hide();
   $("#showLess2").hide();
-  
-$(".scroll").click(function(event){
-		event.preventDefault();
-		$("html,body").animate({scrollTop:$(this.hash).offset().top}, 500);
-		$('.navbar-default a').removeClass('selected');
-		$(this).addClass('selected');
+
+  $(".scroll").click(function (event) {
+    event.preventDefault();
+    $("html,body").animate({ scrollTop: $(this.hash).offset().top }, 500);
+    $(".navbar-default a").removeClass("selected");
+    $(this).addClass("selected");
   });
-  
-  /* clicking 'show details' will turn the button into 'hide details' and vice versa 
-   * 8 projects in total so 16 buttons. 
+
+  /* clicking 'show details' will turn the button into 'hide details' and vice versa
+   * 8 projects in total so 16 buttons.
    */
-  $("#viewMore1").click( function() {
+  $("#viewMore1").click(function () {
     $("#viewMore1").hide();
     $("#showLess1").show();
   });
-  $("#showLess1").click( function () {
+  $("#showLess1").click(function () {
     $("#viewMore1").show();
     $("#showLess1").hide();
   });
-  $("#viewMore2").click( function() {
+  $("#viewMore2").click(function () {
     $("#viewMore2").hide();
     $("#showLess2").show();
   });
-  $("#showLess2").click( function () {
+  $("#showLess2").click(function () {
     $("#viewMore2").show();
     $("#showLess2").hide();
   });
 });
 
-$(document).on('click.bs.collapse.data-api touchstart.bs.collapse.data-api', '[data-toggle="collapse"]', function (e) { 
-});
+$(document).on(
+  "click.bs.collapse.data-api touchstart.bs.collapse.data-api",
+  '[data-toggle="collapse"]',
+  function (e) {}
+);
